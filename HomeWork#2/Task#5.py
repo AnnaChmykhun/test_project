3bb7409a5ad36bfea55203969e4497a69d6fd3b0
Anna Chmykhun <anyutkachv@gmail.com>
2018-04-06 01:25:14 +0300

    Task#5.py: changed to a, b, c = sorted([a, b, c])

--- HomeWork#2/Task#5.py
+++ HomeWork#2/Task#5.py
@@ -2,6 +2,6 @@ a = float(input('Enter number a: '))
 b = float(input('Enter number b: '))
 c = float(input('Enter number c: '))
 
-a, b, c = sorted([a, b, c])[0], sorted([a, b, c])[1], sorted([a, b, c])[2]
+a, b, c = sorted([a, b, c])
 
 print('a = {}; b = {}; c = {}'.format(a, b, c))
