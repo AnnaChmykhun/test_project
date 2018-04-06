3ae1945ed3f197e56ee440b33bb1a31b71cdf886
Anna Chmykhun <anyutkachv@gmail.com>
2018-04-06 11:44:47 +0300

    Task#2.5.py: new solution with using data type Decimal was added

--- HomeWork#2/Task#2.5.py
+++ HomeWork#2/Task#2.5.py
@@ -1,7 +1,16 @@
-n = input('Enter positive float number: ')
-L = len(n)
-index_dot = n.index('.')
-fraction = ''
-for i in range(index_dot + 1, L):
-    fraction += n[i]
-print('Fraction of entered number is 0.{}'.format(fraction))
+# Method#2
+import decimal
+
+n2 = decimal.Decimal(input('Enter positive float number: '))
+fraction = n2 - int(n2)
+print('Fraction of entered number is {}'.format(fraction))
+
+
+# Method#1
+# n1 = input('Enter positive float number: ')
+# L = len(n1)
+# index_dot = n1.index('.')
+# fraction = ''
+# for i in range(index_dot + 1, L):
+#    fraction += n1[i]
+# print('Fraction of entered number is 0.{}'.format(fraction))
